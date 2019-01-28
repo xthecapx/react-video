5ad9aaf8abf8aca046d2d5c675e0e81ad24f8b96
Cristian Marquez <xthecapx@gmail.com>
2019-01-28 15:18:38 -0500

    Updates

---
+++ precache-manifest.fe5e84524852923cb9552da93261fab9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "025a20c942d09779d0f8",
+    "url": "/react-video/static/js/runtime~main.025a20c9.js"
+  },
+  {
+    "revision": "18c3bff64ab24d1d8437",
+    "url": "/react-video/static/js/main.18c3bff6.chunk.js"
+  },
+  {
+    "revision": "4249d3c456f922683d47",
+    "url": "/react-video/static/js/1.4249d3c4.chunk.js"
+  },
+  {
+    "revision": "18c3bff64ab24d1d8437",
+    "url": "/react-video/static/css/main.1d4dac4d.chunk.css"
+  },
+  {
+    "revision": "ba6367515a62850659ab2b70495a48ca",
+    "url": "/react-video/index.html"
+  }
+];
